1e664bbd619c31a675eff4b943bd9a6b5f6d0123
WoKee <791283694@qq.com>
2026-05-15 14:15:10 +0800

    move ed

--- move.bat
+++ move.bat
@@ -1,8 +1,8 @@
 @echo off
 chcp 65001 >nul
-set "source_dir=D:\Project\media\libraries"
-set "target_dir=D:\Project\TV\app\libs"
-set "move_list=D:\Project\media\move.txt"
+set "source_dir=D:\\AndroidStudioProjects\media\libraries"
+set "target_dir=D:\AndroidStudioProjects\TV-MS\app\libs"
+set "move_list=D:\\AndroidStudioProjects\media\move.txt"
 
 for /r "%source_dir%" %%a in (lib-*-release.aar) do (
     findstr /x /c:"%%~nxa" "%move_list%" >nul
